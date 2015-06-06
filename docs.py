14d8fd2f348be5f300176de1cf4a46f54e4bae02
Karel Ha <mathemage@gmail.com>
2015-06-06 10:18:13 +0200

    First version of docs-detection

--- docs.py
+++ docs.py
@@ -23,11 +23,12 @@ def has_docs(owner, repo):
         except requests.exceptions.RequestException:
             # parse Readme file
             #DOCS_REGEX = re.compile('[^a-zA-Z]Documentation[^a-zA-Z]')
+
             DOCS_REGEX = re.compile('### Documentation\s')
             readme = get_readme(owner, repo)
             docs = DOCS_REGEX.search(readme)
 
-            return docs.end() == 0
+            return (docs != None) and (docs.end() != 0)
 '''
             if docs.end() != 0:
                 return True
@@ -35,9 +36,11 @@ def has_docs(owner, repo):
                 docs = DOCS_REGEX.search(readme)
 '''
 
+# tests
 if __name__ == "__main__":
     #print has_docs(sys.argv[1], sys.argv[2])
-    print "mathemage", "Matrixpp",     has_docs("mathemage", "Matrixpp")
-    print "MSusik", "beard",           has_docs("MSusik", "beard")
-    print "MSusik", "matraz",          has_docs("MSusik", "matraz")
-    print "dzenbot", "dznemptydataset",has_docs("dzenbot", "dznemptydataset")
+    print "mathemage", "Matrixpp",     has_docs("mathemage", "Matrixpp")      # docs/
+    print "MSusik", "beard",           has_docs("MSusik", "beard")            # doc/
+    print "MSusik", "matraz",          has_docs("MSusik", "matraz")           # nothing
+    print "dzenbot", "dznemptydataset",has_docs("dzenbot", "dznemptydataset") # we cannot find it, but it's there
+    print "RocketChat", "Rocket.Chat" ,has_docs("RocketChat", "Rocket.Chat")  # "Documentation" in README.md
