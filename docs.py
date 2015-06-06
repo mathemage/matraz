cf1a70f60e77212b081e91416981863bf7eb72a0
Karel Ha <mathemage@gmail.com>
2015-06-06 10:57:32 +0200

    docs-detection: refactor, add more regexes and foldernames for docs/

--- docs.py
+++ docs.py
@@ -8,39 +8,46 @@ import re
 
 from contact import get_readme
 
-def has_docs(owner, repo):
-    url = 'https://github.com/' + owner + '/' + repo + '/tree/master/doc'
-
+def detect_url(url):
     try:
         r = requests.get(url)
         r.raise_for_status()
         return True
     except requests.exceptions.RequestException:
-        try:
-            r = requests.get(url+'s')
-            r.raise_for_status()
+        return False
+
+def search_readme(readme, regex):
+    DOCS_REGEX = re.compile(regex)
+    docs = DOCS_REGEX.search(readme)
+    return (docs != None) and (docs.end() != 0)
+
+def has_docs(owner, repo):
+    prefix = 'https://github.com/' + owner + '/' + repo + '/tree/master/'
+    suffixes = [ 'doc', 'docs', 'Doc', 'Docs', 'documentation',
+            'documentations', 'Documentation', 'Documentations' 'DOC', 'DOCS',
+            'DOCUMENTATION', 'DOCUMENTATIONS' ]
+
+    for suffix in suffixes:
+        if detect_url(prefix + suffix):
             return True
-        except requests.exceptions.RequestException:
-            # parse Readme file
-            #DOCS_REGEX = re.compile('[^a-zA-Z]Documentation[^a-zA-Z]')
 
-            DOCS_REGEX = re.compile('### Documentation\s')
-            readme = get_readme(owner, repo)
-            docs = DOCS_REGEX.search(readme)
+    readme = get_readme(owner, repo)
+    regexes = [ 'doc', 'docs', 'Doc', 'Docs', 'documentation',
+            'documentations', 'Documentation', 'Documentations' 'DOC', 'DOCS',
+            'DOCUMENTATION', 'DOCUMENTATIONS' ]
+    for regex in regexes:
+        if search_readme(readme, '### ' + regex + '\s'):
+            return True
 
-            return (docs != None) and (docs.end() != 0)
-'''
-            if docs.end() != 0:
-                return True
-            else:
-                docs = DOCS_REGEX.search(readme)
-'''
+    return False
 
 # tests
+def run_tests():
+    assert True == has_docs("mathemage", "Matrixpp"), "mathemage/Matrixpp"              # docs/
+    assert True == has_docs("MSusik", "beard"), "MSusik/beard"                          # doc/
+    assert True == has_docs("RocketChat", "Rocket.Chat"), "RocketChat/Rocket.Chat"      # "Documentation" in README.md
+    assert False == has_docs("dzenbot", "dznemptydataset"), "dzenbot/dznemptydataset"   # documention undetected yet available
+    assert False == has_docs("MSusik", "matraz"), "MSusik/matraz"                       # nothing
+
 if __name__ == "__main__":
-    #print has_docs(sys.argv[1], sys.argv[2])
-    print "mathemage", "Matrixpp",     has_docs("mathemage", "Matrixpp")      # docs/
-    print "MSusik", "beard",           has_docs("MSusik", "beard")            # doc/
-    print "MSusik", "matraz",          has_docs("MSusik", "matraz")           # nothing
-    print "dzenbot", "dznemptydataset",has_docs("dzenbot", "dznemptydataset") # we cannot find it, but it's there
-    print "RocketChat", "Rocket.Chat" ,has_docs("RocketChat", "Rocket.Chat")  # "Documentation" in README.md
+    run_tests()
